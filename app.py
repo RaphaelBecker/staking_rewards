d75e874f3a334da3fad5b4a65c8c9c57b85c402e
RaphaelBecker <raphael.becker.private@outlook.de>
2023-01-23 11:34:47 +0100

    not able to plot accumulated_df

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,9 +8,10 @@ def main():
     st.title("CSV to DataFrame")
 
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+    required_columns = ["txid", "refid", "time", "type", "subtype", "aclass", "asset", "amount", "fee", "balance"]
+
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-        required_columns = ["txid","refid","time","type","subtype","aclass","asset","amount","fee","balance"]
 
         if not all(col in df.columns for col in required_columns):
             missing_columns = list(set(required_columns) - set(df.columns))
@@ -23,15 +25,29 @@ def main():
             st.dataframe(df)
 
             assets = df['asset'].unique()
-            for asset in assets:
-                if ".S" in asset:
-                    df_asset = df[df['asset'] == asset]
-                    df_asset = df_asset.set_index('time')
-                    plt.plot(df_asset['amount'])
-                    plt.xlabel('time')
-                    plt.ylabel('amount')
-                    plt.title(asset)
-                    st.pyplot()
+
+            df = df.groupby(['time', 'asset']).sum().reset_index()
+            df = df.pivot(index='time', columns='asset', values='amount')
+            df_accumulated = df.cumsum()
+            st.dataframe(df_accumulated)
+            st.text(df_accumulated.index)
+            st.text(df_accumulated.dtypes)
+
+            columns_to_plot = [col for col in df_accumulated.columns if '.S' in col]
+
+            st.area_chart(df_accumulated, columns_to_plot)
+
+            #assets = df['asset'].unique()
+            #chart = st.line_chart()
+            #for asset in assets:
+            #    if ".S" in asset:
+            #        df_asset = df[df['asset'] == asset]
+            #        df_asset = df_asset.set_index('time')
+            #        plt.plot(df_asset['amount'])
+            #        plt.xlabel('time')
+            #        plt.ylabel('amount')
+
+                    # chart = st.line_chart(data=df_asset['amount'])
 
 
 if __name__ == '__main__':
